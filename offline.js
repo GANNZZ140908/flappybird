f4b4140f80c4eea6a2e4620c01f1fc14fde416b8
GANNZZ140908 <161663668+GANNZZ140908@users.noreply.github.com>
2024-02-29 09:04:23 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1709166892,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg_-sheet0.png",
+		"images/flappyframe-sheet0.png",
+		"images/pipa__1_removebgpreview-sheet0.png",
+		"images/pipa__2_removebgpreview-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
